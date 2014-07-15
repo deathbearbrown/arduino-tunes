c8e7b0ed3b0a9c3e3a5fa4daaa39ca5810640c96
deathbearbrown <deathbearbrown@gmail.com>
2014-07-15 12:30:22 -0400

    updated the tempo... slightly less bad?? maybe

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ int melody[] = {
   NOTE_CS5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_GS4, NOTE_G4, NOTE_F4};
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = { 4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        2, 4, 4, 4, 2, 4, 4, 2};
+int noteDurations[] = { 4, 4, 4, 4, 6, 4, 4, 4, 2, 2,
+                        4, 4, 4, 4, 6, 4, 4, 4, 2, 2,
+                        4, 4, 4, 4, 6, 4, 2, 4, 2, 2,
+                        2, 4, 4, 6, 2, 4, 4, 2};
 ````
--- sailormoon/sailormoon.ino
+++ sailormoon/sailormoon.ino
@@ -24,10 +24,10 @@ int melody[] = {
   NOTE_C4, NOTE_F4,NOTE_GS4, NOTE_C5, NOTE_C5, NOTE_AS4, NOTE_AS4, NOTE_C5, NOTE_DS5, NOTE_CS5,
   NOTE_CS5, NOTE_C5, NOTE_AS4, NOTE_C5, NOTE_AS4, NOTE_GS4, NOTE_G4, NOTE_F4};
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = { 4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        4, 4, 4, 4, 4, 4, 4, 4, 2, 2,
-                        2, 4, 4, 4, 2, 4, 4, 2};
+int noteDurations[] = { 4, 4, 4, 4, 6, 4, 4, 4, 2, 2,
+                        4, 4, 4, 4, 6, 4, 4, 4, 2, 2,
+                        4, 4, 4, 4, 6, 4, 2, 4, 2, 2,
+                        2, 4, 4, 6, 2, 4, 4, 2};
 
 void setup() {
   // iterate over the notes of the melody:
